947a5bc24ccef45e5263ca7d962a2461e07d29ac
KhaetbekIT <hayotbek-web@mail.ru>
2023-08-14 12:18:30 +0500

    #fix #bug

--- dist/main.js
+++ dist/main.js
@@ -16,7 +16,7 @@
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_api__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/api */ \"./src/modules/api.js\");\n/* harmony import */ var _modules_card__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/card */ \"./src/modules/card.js\");\n/* harmony import */ var _modules_render_goods__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/render-goods */ \"./src/modules/render-goods.js\");\n\n\n\n\n(0,_modules_card__WEBPACK_IMPORTED_MODULE_1__[\"default\"])()\n\n;(0,_modules_api__WEBPACK_IMPORTED_MODULE_0__.GetDataFunc)().then(data =>{\n    ;(0,_modules_render_goods__WEBPACK_IMPORTED_MODULE_2__[\"default\"])(data)\n})\n\n//# sourceURL=webpack://ozon/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_api__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/api */ \"./src/modules/api.js\");\n/* harmony import */ var _modules_card__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/card */ \"./src/modules/card.js\");\nObject(function webpackMissingModule() { var e = new Error(\"Cannot find module './modules/render-goods'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\n\n\n\n\n(0,_modules_card__WEBPACK_IMPORTED_MODULE_1__[\"default\"])()\n\n;(0,_modules_api__WEBPACK_IMPORTED_MODULE_0__.GetDataFunc)().then(data =>{\n    Object(function webpackMissingModule() { var e = new Error(\"Cannot find module './modules/render-goods'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(data)\n})\n\n//# sourceURL=webpack://ozon/./src/index.js?");
 
 /***/ }),
 
@@ -37,16 +37,6 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst CartInit = () => {\n    const cart = document.querySelector(\"#cart\")\n    const cartModal = document.querySelector(\"#cart-js\")\n    const cartModalClose = cartModal.querySelector(\".cart-close\")\n\n    cart.addEventListener(\"click\", () => {\n        cartModal.classList.add(\"d-flex\")\n    })\n\n    cartModalClose.addEventListener(\"click\", () => {\n        cartModal.classList.remove(\"d-flex\")\n    })\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (CartInit);\n\n//# sourceURL=webpack://ozon/./src/modules/card.js?");
-
-/***/ }),
-
-/***/ "./src/modules/render-goods.js":
-/*!*************************************!*\
-  !*** ./src/modules/render-goods.js ***!
-  \*************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst RenderGoodsInit = (goods = []) => {\n\n    const goodsContainer = document.querySelector(\".goods\")\n\n    goodsContainer.innerHTML = ''\n\n    goods.forEach(goodsItem => {\n        goodsContainer.insertAdjacentHTML(\"beforeend\", `\n            <!-- КАРТОЧКА ТОВАРА -->\n            <div class=\"col-12 col-md-6 col-lg-4 col-xl-3\">\n                <div class=\"card\">\n                    ${goodsItem.sale ? (\n                `<div class=\"card-sale\">🔥Hot Sale🔥</div>`\n            ) : (\"\")\n            }\n                    <div class=\"card-img-wrapper\">\n                        <span class=\"card-img-top\"\n                            style=\"background-image: url('${goodsItem.img}')\"></span>\n                    </div>\n                    <div class=\"card-body justify-content-between\">\n                        <div class=\"card-price\">${goodsItem.price} ₽</div>\n                        <h5 class=\"card-title\">${goodsItem.title} </h5>\n                        <button class=\"btn btn-primary\">В корзину</button>\n                    </div>\n                </div>\n            </div>\n            <!-- END КАРТОЧКА ТОВАРА -->\n        `)\n    })\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (RenderGoodsInit);\n\n//# sourceURL=webpack://ozon/./src/modules/render-goods.js?");
 
 /***/ })
 

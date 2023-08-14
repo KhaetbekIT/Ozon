5151d144988d2e6d047bbfbaedb35b5b7985bd11
KhaetbekIT <hayotbek-web@mail.ru>
2023-08-14 12:23:43 +0500

    #fix #default-merge

--- dist/main.js
+++ dist/main.js
@@ -16,91 +16,118 @@
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_api__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/api */ \"./src/modules/api.js\");\n/* harmony import */ var _modules_card__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/card */ \"./src/modules/card.js\");\nObject(function webpackMissingModule() { var e = new Error(\"Cannot find module './modules/render-goods'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\n\n\n\n\n(0,_modules_card__WEBPACK_IMPORTED_MODULE_1__[\"default\"])()\n\n;(0,_modules_api__WEBPACK_IMPORTED_MODULE_0__.GetDataFunc)().then(data =>{\n    Object(function webpackMissingModule() { var e = new Error(\"Cannot find module './modules/render-goods'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(data)\n})\n\n//# sourceURL=webpack://ozon/./src/index.js?");
+        eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_api__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/api */ \"./src/modules/api.js\");\n/* harmony import */ var _modules_card__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/card */ \"./src/modules/card.js\");\n/* harmony import */ var _modules_render_goods__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/render-goods */ \"./src/modules/render-goods.js\");\n\n\n\n\n(0,_modules_card__WEBPACK_IMPORTED_MODULE_1__[\"default\"])()\n\n;(0,_modules_api__WEBPACK_IMPORTED_MODULE_0__.GetDataFunc)().then(data =>{\n    ;(0,_modules_render_goods__WEBPACK_IMPORTED_MODULE_2__[\"default\"])(data)\n})\n\n//# sourceURL=webpack://ozon/./src/index.js?");
 
-/***/ }),
+        /***/
+}),
 
-/***/ "./src/modules/api.js":
-/*!****************************!*\
-  !*** ./src/modules/api.js ***!
-  \****************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+  /***/ "./src/modules/api.js":
+  /*!****************************!*\
+    !*** ./src/modules/api.js ***!
+    \****************************/
+  /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   DeleteDataFunc: () => (/* binding */ DeleteDataFunc),\n/* harmony export */   GetDataFunc: () => (/* binding */ GetDataFunc),\n/* harmony export */   PostDataFunc: () => (/* binding */ PostDataFunc)\n/* harmony export */ });\nconst GetDataFunc = (path = \"\") => {\n    return fetch(`https://khaetbek-ozon-ab945-default-rtdb.firebaseio.com/goods.json/${path}`)\n        .then(response => response.json())\n\n}\n\nconst PostDataFunc = (path = \"\", data = {}) => {\n    return fetch(`https://khaetbek-ozon-ab945-default-rtdb.firebaseio.com/goods.json/${path}`, {\n        method: \"POST\",\n        body: JSON.stringify(data),\n        headers: {\n            \"Context-type\": \"application/json\"\n        }\n    })\n}\n\nconst DeleteDataFunc = (path = \"\", id = 0) => {\n    return fetch(`https://khaetbek-ozon-ab945-default-rtdb.firebaseio.com/goods.json/${path}/${id}`)\n}\n\n\n\n//# sourceURL=webpack://ozon/./src/modules/api.js?");
+        eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   DeleteDataFunc: () => (/* binding */ DeleteDataFunc),\n/* harmony export */   GetDataFunc: () => (/* binding */ GetDataFunc),\n/* harmony export */   PostDataFunc: () => (/* binding */ PostDataFunc)\n/* harmony export */ });\nconst GetDataFunc = (path = \"\") => {\n    return fetch(`https://khaetbek-ozon-ab945-default-rtdb.firebaseio.com/goods.json/${path}`)\n        .then(response => response.json())\n\n}\n\nconst PostDataFunc = (path = \"\", data = {}) => {\n    return fetch(`https://khaetbek-ozon-ab945-default-rtdb.firebaseio.com/goods.json/${path}`, {\n        method: \"POST\",\n        body: JSON.stringify(data),\n        headers: {\n            \"Context-type\": \"application/json\"\n        }\n    })\n}\n\nconst DeleteDataFunc = (path = \"\", id = 0) => {\n    return fetch(`https://khaetbek-ozon-ab945-default-rtdb.firebaseio.com/goods.json/${path}/${id}`)\n}\n\n\n\n//# sourceURL=webpack://ozon/./src/modules/api.js?");
 
-/***/ }),
+        /***/
+      }),
 
-/***/ "./src/modules/card.js":
-/*!*****************************!*\
-  !*** ./src/modules/card.js ***!
-  \*****************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+  /***/ "./src/modules/card.js":
+  /*!*****************************!*\
+    !*** ./src/modules/card.js ***!
+    \*****************************/
+  /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst CartInit = () => {\n    const cart = document.querySelector(\"#cart\")\n    const cartModal = document.querySelector(\"#cart-js\")\n    const cartModalClose = cartModal.querySelector(\".cart-close\")\n\n    cart.addEventListener(\"click\", () => {\n        cartModal.classList.add(\"d-flex\")\n    })\n\n    cartModalClose.addEventListener(\"click\", () => {\n        cartModal.classList.remove(\"d-flex\")\n    })\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (CartInit);\n\n//# sourceURL=webpack://ozon/./src/modules/card.js?");
+        eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst CartInit = () => {\n    const cart = document.querySelector(\"#cart\")\n    const cartModal = document.querySelector(\"#cart-js\")\n    const cartModalClose = cartModal.querySelector(\".cart-close\")\n\n    cart.addEventListener(\"click\", () => {\n        cartModal.classList.add(\"d-flex\")\n    })\n\n    cartModalClose.addEventListener(\"click\", () => {\n        cartModal.classList.remove(\"d-flex\")\n    })\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (CartInit);\n\n//# sourceURL=webpack://ozon/./src/modules/card.js?");
 
-/***/ })
+        /***/
+      }),
 
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
-/******/ 	
-/******/ })()
-;
+  /***/ "./src/modules/render-goods.js":
+  /*!*************************************!*\
+    !*** ./src/modules/render-goods.js ***!
+    \*************************************/
+  /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+        eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst RenderGoodsInit = (goods = []) => {\n\n    const goodsContainer = document.querySelector(\".goods\")\n\n    goodsContainer.innerHTML = ''\n\n    goods.forEach(goodsItem => {\n        goodsContainer.insertAdjacentHTML(\"beforeend\", `\n            <!-- КАРТОЧКА ТОВАРА -->\n            <div class=\"col-12 col-md-6 col-lg-4 col-xl-3\">\n                <div class=\"card\">\n                    ${goodsItem.sale ? (\n                `<div class=\"card-sale\">🔥Hot Sale🔥</div>`\n            ) : (\"\")\n            }\n                    <div class=\"card-img-wrapper\">\n                        <span class=\"card-img-top\"\n                            style=\"background-image: url('${goodsItem.img}')\"></span>\n                    </div>\n                    <div class=\"card-body justify-content-between\">\n                        <div class=\"card-price\">${goodsItem.price} ₽</div>\n                        <h5 class=\"card-title\">${goodsItem.title} </h5>\n                        <button class=\"btn btn-primary\">В корзину</button>\n                    </div>\n                </div>\n            </div>\n            <!-- END КАРТОЧКА ТОВАРА -->\n        `)\n    })\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (RenderGoodsInit);\n\n//# sourceURL=webpack://ozon/./src/modules/render-goods.js?");
+
+        /***/
+      })
+
+    /******/
+  });
+  /************************************************************************/
+  /******/ 	// The module cache
+  /******/ 	var __webpack_module_cache__ = {};
+  /******/
+  /******/ 	// The require function
+  /******/ 	function __webpack_require__(moduleId) {
+  /******/ 		// Check if module is in cache
+  /******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+  /******/ 		if (cachedModule !== undefined) {
+  /******/ 			return cachedModule.exports;
+      /******/
+    }
+  /******/ 		// Create a new module (and put it into the cache)
+  /******/ 		var module = __webpack_module_cache__[moduleId] = {
+  /******/ 			// no module.id needed
+  /******/ 			// no module.loaded needed
+  /******/ 			exports: {}
+      /******/
+    };
+  /******/
+  /******/ 		// Execute the module function
+  /******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+  /******/
+  /******/ 		// Return the exports of the module
+  /******/ 		return module.exports;
+    /******/
+  }
+  /******/
+  /************************************************************************/
+  /******/ 	/* webpack/runtime/define property getters */
+  /******/ 	(() => {
+  /******/ 		// define getter functions for harmony exports
+  /******/ 		__webpack_require__.d = (exports, definition) => {
+  /******/ 			for (var key in definition) {
+  /******/ 				if (__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+  /******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+          /******/
+        }
+        /******/
+      }
+      /******/
+    };
+    /******/
+  })();
+  /******/
+  /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+  /******/ 	(() => {
+  /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+    /******/
+  })();
+  /******/
+  /******/ 	/* webpack/runtime/make namespace object */
+  /******/ 	(() => {
+  /******/ 		// define __esModule on exports
+  /******/ 		__webpack_require__.r = (exports) => {
+  /******/ 			if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+  /******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+        /******/
+      }
+  /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+      /******/
+    };
+    /******/
+  })();
+  /******/
+  /************************************************************************/
+  /******/
+  /******/ 	// startup
+  /******/ 	// Load entry module and return exports
+  /******/ 	// This entry module can't be inlined because the eval devtool is used.
+  /******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+  /******/
+  /******/
+})()
+  ;

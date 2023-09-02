cc5f29cd901a473d573ca5dc978590feea3036c6
KhaetbekIT <hayotbek-web@mail.ru>
2023-09-02 12:05:03 +0500

    #fix #firebase #js #Src ФИкс обновление базы и рендер данных

--- src/modules/render-goods.js
+++ src/modules/render-goods.js
@@ -6,29 +6,26 @@ const RenderGoodsInit = (goods = []) => {
 
     goodsContainer.innerHTML = ''
 
-    goods.forEach(goodsItem => {
-        if (goodsItem) {
+    goods.forEach(item => {
+        if (item) {
             goodsContainer.insertAdjacentHTML("beforeend", `
-            <!-- КАРТОЧКА ТОВАРА -->
-            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
-                <div class="card" data-key="${goodsItem.id}">
-                    ${goodsItem.sale ? (
-                    `<div class="card-sale">🔥Hot Sale🔥</div>`
-                ) : ("")
-                }
-                    <div class="card-img-wrapper">
-                        <span class="card-img-top"
-                            style="background-image: url('${goodsItem.img}')"></span>
-                    </div>
-                    <div class="card-body justify-content-between">
-                        <div class="card-price">${goodsItem.price} ₽</div>
-                        <h5 class="card-title">${goodsItem.title} </h5>
-                        <button class="btn btn-primary">В корзину</button>
+                <!-- КАРТОЧКА ТОВАРА -->
+                <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                        <div class="card" data-key="${item.id}">
+                            ${item.sale ? (`<div class="card-sale">🔥Hot Sale🔥</div>`) : ("")}
+                        <div class="card-img-wrapper">
+                            <span class="card-img-top"
+                                style="background-image: url('${item.img}')"></span>
+                        </div>
+                        <div class="card-body justify-content-between">
+                            <div class="card-price">${item.price} ₽</div>
+                            <h5 class="card-title">${item.title} </h5>
+                            <button class="btn btn-primary">В корзину</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <!-- END КАРТОЧКА ТОВАРА -->
-        `)
+                <!-- END КАРТОЧКА ТОВАРА -->
+            `)
         }
     })
 }
